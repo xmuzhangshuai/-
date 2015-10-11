cbc4091ae691b8d52cc567ccf59e83c003652f1c
liuweiqiang <512287450@qq.com>
2015-10-11 12:12:40 +0800

    superpower源码

---
+++ SuperPower/data/config.php
@@ -0,0 +1,36 @@
+<?php
+defined('IN_IA') or exit('Access Denied');
+
+$config = array();
+
+$config['db']['host'] = '127.0.0.1';
+$config['db']['username'] = 'root';
+$config['db']['password'] = 'e23456';
+$config['db']['port'] = '3306';
+$config['db']['database'] = 'weqin';
+$config['db']['charset'] = 'utf8';
+$config['db']['pconnect'] = 0;
+$config['db']['tablepre'] = 'ims_';
+
+// --------------------------  CONFIG COOKIE  --------------------------- //
+$config['cookie']['pre'] = '3de1_';
+$config['cookie']['domain'] = '';
+$config['cookie']['path'] = '/';
+
+// --------------------------  CONFIG SETTING  --------------------------- //
+$config['setting']['charset'] = 'utf-8';
+$config['setting']['cache'] = 'mysql';
+$config['setting']['timezone'] = 'Asia/Shanghai';
+$config['setting']['memory_limit'] = '256M';
+$config['setting']['filemode'] = 0644;
+$config['setting']['authkey'] = '044030e3';
+$config['setting']['founder'] = '1';
+$config['setting']['development'] = 0;
+$config['setting']['referrer'] = 0;
+
+// --------------------------  CONFIG UPLOAD  --------------------------- //
+$config['upload']['image']['extentions'] = array('gif', 'jpg', 'jpeg', 'png');
+$config['upload']['image']['limit'] = 5000;
+$config['upload']['attachdir'] = 'attachment';
+$config['upload']['audio']['extentions'] = array('mp3');
+$config['upload']['audio']['limit'] = 5000;
